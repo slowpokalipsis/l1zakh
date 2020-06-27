7f51816556d176a340560cc2750452991e7ef8cc
slowpokalipsis <slowpokalipsis@gmail.com>
2020-06-27 13:25:36 +1000

    Fixed FR

--- recognition.py
+++ recognition.py
@@ -3,7 +3,6 @@ import cv2
 import numpy as np
 import os
 import pickle
-import glob
 
 from pymongo import MongoClient as mc
 myclient = mc("mongodb://localhost:27017/")
@@ -21,7 +20,7 @@ cursor = col.find()
 len = col.find().count()
 
 for i in range(len):
-    a = [pickle.loads(cursor[i]['photo'])]
+    a = pickle.loads(cursor[i]['photo'])
     known_face_encodings.append(a)
     known_face_names.append(cursor[i]['name'])
 
@@ -34,8 +33,6 @@ face_names = []
 process_this_frame = True
 
 video_capture = cv2.VideoCapture(0)
-print(known_face_names)
-print(known_face_encodings)
 while True:
     # Grab a single frame of video
     ret, frame = video_capture.read()
@@ -52,44 +49,33 @@ while True:
         face_locations = face_recognition.face_locations(rgb_small_frame)
         face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
 
-        print('face_encodings')
-        print(face_encodings)
-        print('1')
         face_names = []
         for face_encoding in face_encodings:
-            print('face_encoding')
-            print(face_encoding)
             # See if the face is a match for the known face(s)
             matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
             name = "Unknown"
-            print('matches')
-            print(matches)
             face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
-            print(face_distances)
             best_match_index = np.argmin(face_distances)
-            print(best_match_index)
             if matches[best_match_index]:
                 name = known_face_names[best_match_index]
 
             face_names.append(name)
-            print(face_locations)
-            print(face_names)
 
     process_this_frame = not process_this_frame
 
     # Display the results
 
     for (top, right, bottom, left), name in zip(face_locations, face_names):
-        print('aue')
         # Scale back up face locations since the frame we detected in was scaled to 1/4 size
         top *= 4
         right *= 4
         bottom *= 4
         left *= 4
 
-        # dirname = os.path.dirname(__file__) + '\known_people'
-        # if name != 'Unknown':
-        #     name = name[:-4].replace(dirname, '')[1:]
+        dirname = os.path.dirname(__file__) + '/known_people'
+        
+        if name != 'Unknown':
+            name = name[:-4].replace(dirname, '')[1:]
 
         # Draw a box around the face
         cv2.rectangle(frame, (left, top), (right, bottom), (133, 133, 133), 2)
@@ -97,11 +83,6 @@ while True:
         # Draw a label with a name below the face
         cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (133, 133, 133), cv2.FILLED)
         font = cv2.FONT_ITALIC
-        print(name)
-        print('cycle')
-        print()
-        print()
-        print()
         cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (0, 0, 0), 1)
 
     # Display the resulting image
